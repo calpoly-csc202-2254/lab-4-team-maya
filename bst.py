2904015f2602728ec41b255089a4473847b85221
Maya <132492526+mseagraves@users.noreply.github.com>
2025-05-19 22:58:55 +0000

    Data Definitions

--- bst.py
+++ bst.py
@@ -4,6 +4,26 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
+# Data Definitions
+
+BinTree : TypeAlias = Union['Node', None]
+
+@dataclass(frozen = True)
+class Node:
+    val : any
+    left : BinTree
+    right : BinTree
+
+@dataclass(frozen = True)
+class BinarySearchTree:
+    val : any
+    left : BinTree
+    right : BinTree
+    comes_before : Callable[[Node, Node], bool]
+
+# Functions
 
 def example_fun(x : int) -> bool:
     return x < 142
+
+# Test Cases
