5a8f7666cad3871ca6362b09086e487ae04c4530
Maya <132492526+mseagraves@users.noreply.github.com>
2025-05-22 05:25:20 +0000

    Completed All Functions with Test Cases

--- bst.py
+++ bst.py
@@ -16,14 +16,166 @@ class Node:
 
 @dataclass(frozen = True)
 class BinarySearchTree:
-    val : any
-    left : BinTree
-    right : BinTree
-    comes_before : Callable[[Node, Node], bool]
+    comes_before : Callable[[any, any], bool]
+    tree : BinTree
 
 # Functions
 
-def example_fun(x : int) -> bool:
-    return x < 142
+# returns True if a binary search tree is empty
+def is_empty(bst : BinarySearchTree) -> bool:
+    match bst.tree:
+        case None:
+            return True
+        case Node:
+            return False  
+
+# inserts a value into a binary tree using a comes_before function
+def insert_helper(bt : BinTree, new_val : any, comes_before : Callable[[any, any], bool]) -> BinTree:
+    match bt:
+        case None:
+            return Node(new_val, None, None)
+        case Node(v, l, r):
+            if comes_before(new_val, v):
+                return Node(v, insert_helper(l, new_val, comes_before), r)
+            else:
+                return Node(v, l, insert_helper(r, new_val, comes_before))
+
+# inserts a value into a binary search tree
+def insert(bst : BinarySearchTree, new_val : any) -> BinarySearchTree:
+    return BinarySearchTree(bst.comes_before, insert_helper(bst.tree, new_val, bst.comes_before))
+
+# uses the ordering function of a binary search tree to look for a specified value
+def lookup_helper(bt : BinTree, val : any, comes_before : Callable[[any, any], bool]) -> bool:
+    match bt:
+        case None:
+            return False
+        case Node(v, l, r):
+            if comes_before(val, v):
+                return lookup_helper(l, val, comes_before)
+            elif comes_before(v, val):
+                return lookup_helper(r, val, comes_before)
+            else:
+                return True
+
+# returns True if a value is stored in a binary search tree and False otherwise
+def lookup(bst: BinarySearchTree, val : any) -> bool:
+    return lookup_helper(bst.tree, val, bst.comes_before)
+
+# returns a the largest value in a binary tree
+def largest(bt : BinTree) -> any:
+    match bt:
+        case None:
+            raise ValueError('No largest value in an empty tree')
+        case Node(v, l, r):
+            if r is None:
+                return v
+            else:
+                return largest(r)
+
+# returns a binary tree without the largest value
+def rem_largest(bt : BinTree) -> BinTree:
+    match bt:
+        case None:
+            raise ValueError('Cannot remove the largest value from an empty tree')
+        case Node(v, l, r):
+            if r is None:
+                return l
+            else:
+                return Node(v, l, rem_largest(r))
+
+# removes a value from a binary tree and replaces it with the largest value to the left
+def delete_helper(bt : BinTree, val : any, comes_before : Callable[[any, any], bool]) -> BinTree:
+    match bt:
+        case None:
+            raise ValueError('Value is not in Binary Search Tree')
+        case Node(v, l, r):
+            if comes_before(val, v):
+                return Node(v, delete_helper(l, val, comes_before), r)
+            elif comes_before(v, val):
+                return Node(v, l, delete_helper(r, val, comes_before))
+            else:
+                if l is None:
+                    return r
+                else:
+                    return Node(largest(l), rem_largest(l), r)
+
+# removes a value from a binary search tree and returns the resulting binary search tree
+def delete(bst : BinarySearchTree, val : any) -> BinarySearchTree:
+    if lookup(bst, val):
+        return BinarySearchTree(bst.comes_before, delete_helper(bst.tree, val, bst.comes_before))
+    else:
+        raise ValueError('Value is not in Binary Search Tree')
 
 # Test Cases
+class Tests(unittest.TestCase):
+    def test_is_empty(self):
+        def less_than(x : int, y : int) -> bool:
+            return x < y
+        
+        test_bstree_empty = BinarySearchTree(less_than, None)
+        test_bstree_nonempty = BinarySearchTree(less_than, Node(1, None, Node(2, None, None)))
+
+        self.assertTrue(is_empty(test_bstree_empty))
+        self.assertFalse(is_empty(test_bstree_nonempty))
+    
+    def test_insert(self):
+        def less_than(x : int, y : int) -> bool:
+            return x < y
+
+        test_bstree_none = BinarySearchTree(less_than, None)
+        test_bstree = BinarySearchTree(less_than, Node(1, None, Node(3, None, None)))
+
+        bstree_none_result = BinarySearchTree(less_than, Node(0, None, None))
+        bstree_0_result = BinarySearchTree(less_than, Node(1, Node(0, None, None), Node(3, None, None)))
+        bstree_2_result = BinarySearchTree(less_than, Node(1, None, Node(3, Node(2, None, None), None)))
+        bstree_4_result = BinarySearchTree(less_than, Node(1, None, Node(3, None, Node(4, None, None))))
+
+        self.assertEqual(bstree_none_result, insert(test_bstree_none, 0))
+        self.assertEqual(bstree_0_result, insert(test_bstree, 0))
+        self.assertEqual(bstree_2_result, insert(test_bstree, 2))
+        self.assertEqual(bstree_4_result, insert(test_bstree, 4))
+    
+    def test_lookup(self):
+        def less_than(x : int, y : int) -> bool:
+            return x < y
+        
+        test_bstree = BinarySearchTree(less_than, Node(1, None, Node(3, None, Node(4, None, None))))
+
+        self.assertTrue(lookup(test_bstree, 1))
+        self.assertTrue(lookup(test_bstree, 3))
+        self.assertTrue(lookup(test_bstree, 4))
+        self.assertFalse(lookup(test_bstree, 0))
+    
+    def test_largest(self):
+        bintree = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        
+        self.assertEqual(12, largest(bintree))
+        self.assertEqual(6, largest(bintree.left))
+    
+    def test_rem_largest(self):
+        bintree1 = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        bintree1_res = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), None))
+        
+        bintree2 = Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None))
+        bintree2_res = Node(5, Node(3, Node(2, None, None), None), Node(4, None, None))
+
+        self.assertEqual(bintree1_res, rem_largest(bintree1))
+        self.assertEqual(bintree2_res, rem_largest(bintree2))
+    
+    def test_delete(self):
+        def less_than(x : int, y : int) -> bool:
+            return x < y
+
+        bintree = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+
+        bintree_rem2 = Node(7, Node(5, Node(3, None, None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        bintree_rem7 = Node(6, Node(5, Node(3, Node(2, None, None), None), Node(4, None, None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        bintree_rem10 = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(9, Node(8, None, None), Node(12, None, None)))
+
+        self.assertEqual(BinarySearchTree(less_than, bintree_rem2), delete(BinarySearchTree(less_than, bintree), 2))
+        self.assertEqual(BinarySearchTree(less_than, bintree_rem7), delete(BinarySearchTree(less_than, bintree), 7))
+        self.assertEqual(BinarySearchTree(less_than, bintree_rem10), delete(BinarySearchTree(less_than, bintree), 10))
+
+if (__name__ == '__main__'):
+    unittest.main()
+
--- bst_tests.py
+++ bst_tests.py
@@ -6,10 +6,136 @@ sys.setrecursionlimit(10**6)
 
 from bst import *
 
+# Data Definitions
+
+@dataclass
+class XYCoords:
+    x : float
+    y : float
+
+def num_ord(x : float, y : float) -> bool:
+    return x < y
+
+def str_ord(a : str, b : str) -> bool:
+    return a < b
+
+def euc_dist(c : XYCoords) -> float:
+    return ((c.x ** 2) + (c.y ** 2)) ** (0.5)
+
+def dist_ord(c1 : XYCoords, c2 : XYCoords) -> bool:
+    return euc_dist(c1) < euc_dist(c2)
+
+# Test Cases
+
 class BSTTests(unittest.TestCase):
-    def test_example_fun(self):
-        self.assertEqual(True, example_fun(34))
-        self.assertEqual(False,example_fun(1423))
+    def test_is_empty(self):
+        nbst_empty = BinarySearchTree(num_ord, None)
+        nbst_nonempty = BinarySearchTree(num_ord, Node(1, None, Node(2, None, None)))
+
+        self.assertTrue(is_empty(nbst_empty))
+        self.assertFalse(is_empty(nbst_nonempty))
+
+        sbst_empty = BinarySearchTree(str_ord, None)
+        sbst_nonempty = BinarySearchTree(str_ord, Node('a', None, Node('b', None, None)))
+
+        self.assertTrue(is_empty(sbst_empty))
+        self.assertFalse(is_empty(sbst_nonempty))
+
+        cbst_empty = BinarySearchTree(dist_ord, None)
+        cbst_nonempty = BinarySearchTree(dist_ord, Node(XYCoords(3, 4), None, Node(XYCoords(10, 8), None, None)))
+
+        self.assertTrue(is_empty(cbst_empty))
+        self.assertFalse(is_empty(cbst_nonempty))
+    
+    def test_insert(self):
+        num_bstree_none = BinarySearchTree(num_ord, None)
+        num_bstree = BinarySearchTree(num_ord, Node(1, None, Node(3, None, None)))
+
+        num_bstree_none_result = BinarySearchTree(num_ord, Node(0, None, None))
+        bstree_0_result = BinarySearchTree(num_ord, Node(1, Node(0, None, None), Node(3, None, None)))
+        bstree_2_result = BinarySearchTree(num_ord, Node(1, None, Node(3, Node(2, None, None), None)))
+        bstree_4_result = BinarySearchTree(num_ord, Node(1, None, Node(3, None, Node(4, None, None))))
+
+        self.assertEqual(num_bstree_none_result, insert(num_bstree_none, 0))
+        self.assertEqual(bstree_0_result, insert(num_bstree, 0))
+        self.assertEqual(bstree_2_result, insert(num_bstree, 2))
+        self.assertEqual(bstree_4_result, insert(num_bstree, 4))
+
+        str_bstree_none = BinarySearchTree(str_ord, None)
+        str_bstree = BinarySearchTree(str_ord, Node('acdfev', None, Node('dog', None, None)))
+
+        str_bstree_none_result = BinarySearchTree(str_ord, Node('abc', None, None))
+        bstree_abc_result = BinarySearchTree(str_ord, Node('acdfev', Node('abc', None, None), Node('dog', None, None)))
+        bstree_bigger_result = BinarySearchTree(str_ord, Node('acdfev', None, Node('dog', Node('bigger', None, None), None)))
+        bstree_zebra_result = BinarySearchTree(str_ord, Node('acdfev', None, Node('dog', None, Node('zebra', None, None))))
+
+        self.assertEqual(str_bstree_none_result, insert(str_bstree_none, 'abc'))
+        self.assertEqual(bstree_abc_result, insert(str_bstree, 'abc'))
+        self.assertEqual(bstree_bigger_result, insert(str_bstree, 'bigger'))
+        self.assertEqual(bstree_zebra_result, insert(str_bstree, 'zebra'))
+
+        dist_bstree_none = BinarySearchTree(dist_ord, None)
+        dist_bstree = BinarySearchTree(dist_ord, Node(XYCoords(-2, 3), None, Node(XYCoords(-5, -5), None, None)))
+
+        dist_bstree_none_result = BinarySearchTree(dist_ord, Node(XYCoords(1, -1), None, None))
+        bstree_result1 = BinarySearchTree(dist_ord, Node(XYCoords(-2, 3), Node(XYCoords(1, -1), None, None), Node(XYCoords(-5, -5), None, None)))
+        bstree_result2 = BinarySearchTree(dist_ord, Node(XYCoords(-2, 3), None, Node(XYCoords(-5, -5), Node(XYCoords(4, 3), None, None), None)))
+        bstree_result3 = BinarySearchTree(dist_ord, Node(XYCoords(-2, 3), None, Node(XYCoords(-5, -5), None, Node(XYCoords(-10, 12), None, None))))
+
+        self.assertEqual(dist_bstree_none_result, insert(dist_bstree_none, XYCoords(1, -1)))
+        self.assertEqual(bstree_result1, insert(dist_bstree, XYCoords(1, -1)))
+        self.assertEqual(bstree_result2, insert(dist_bstree, XYCoords(4, 3)))
+        self.assertEqual(bstree_result3, insert(dist_bstree, XYCoords(-10, 12)))
+    
+    def test_lookup(self):
+        num_bstree = BinarySearchTree(num_ord, Node(1, None, Node(3, None, Node(4, None, None))))
+        str_bstree = BinarySearchTree(str_ord, Node('acdfev', None, Node('dog', None, None)))
+        dist_bstree = BinarySearchTree(dist_ord, Node(XYCoords(-2, 3), None, Node(XYCoords(-5, -5), None, None)))
+
+        self.assertTrue(lookup(num_bstree, 1))
+        self.assertFalse(lookup(num_bstree, 0))
+
+        self.assertTrue(lookup(str_bstree, 'dog'))
+        self.assertFalse(lookup(str_bstree, 'cat'))
+
+        self.assertTrue(lookup(dist_bstree, XYCoords(-2, 3)))
+        self.assertFalse(lookup(dist_bstree, XYCoords(1, 1)))
+    
+    def test_largest(self):
+        num_bintree = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        str_bintree = Node('food', Node('dog', Node('bingo', Node('apple', None, None), None), Node('ear', Node('cat', None, None), None)), Node('yellow', Node('georgia', None, Node('lemon', None, None)), Node('zebra', None, None)))
+        dist_bintree = Node(XYCoords(6, 8), Node(XYCoords(-5, 4), Node(XYCoords(-2, 1), Node(XYCoords(1, -1), None, None), None), Node(XYCoords(-7, -6), Node(XYCoords(3, 4), None, None), None)), Node(XYCoords(-20, -25), Node(XYCoords(10, -11), None, Node(XYCoords(15, 14), None, None)), Node(XYCoords(100, -50), None, None)))
+
+        self.assertEqual(12, largest(num_bintree))
+        self.assertEqual('zebra', largest(str_bintree))
+        self.assertEqual(XYCoords(100, -50), largest(dist_bintree))
+    
+    def test_rem_largest(self):
+        num_bintree = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        str_bintree = Node('food', Node('dog', Node('bingo', Node('apple', None, None), None), Node('ear', Node('cat', None, None), None)), Node('yellow', Node('georgia', None, Node('lemon', None, None)), Node('zebra', None, None)))
+        dist_bintree = Node(XYCoords(6, 8), Node(XYCoords(-5, 4), Node(XYCoords(-2, 1), Node(XYCoords(1, -1), None, None), None), Node(XYCoords(-7, -6), Node(XYCoords(3, 4), None, None), None)), Node(XYCoords(-20, -25), Node(XYCoords(10, -11), None, Node(XYCoords(15, 14), None, None)), Node(XYCoords(100, -50), None, None)))
+
+        num_bintree_res = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), None))
+        str_bintree_res = Node('food', Node('dog', Node('bingo', Node('apple', None, None), None), Node('ear', Node('cat', None, None), None)), Node('yellow', Node('georgia', None, Node('lemon', None, None)), None))
+        dist_bintree_res = Node(XYCoords(6, 8), Node(XYCoords(-5, 4), Node(XYCoords(-2, 1), Node(XYCoords(1, -1), None, None), None), Node(XYCoords(-7, -6), Node(XYCoords(3, 4), None, None), None)), Node(XYCoords(-20, -25), Node(XYCoords(10, -11), None, Node(XYCoords(15, 14), None, None)), None))
+
+        self.assertEqual(num_bintree_res, rem_largest(num_bintree))
+        self.assertEqual(str_bintree_res, rem_largest(str_bintree))
+        self.assertEqual(dist_bintree_res, rem_largest(dist_bintree))
+    
+    def test_delete(self):
+        num_bintree = Node(7, Node(5, Node(3, Node(2, None, None), None), Node(6, Node(4, None, None), None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        str_bintree = Node('food', Node('dog', Node('bingo', Node('apple', None, None), None), Node('ear', Node('cat', None, None), None)), Node('yellow', Node('georgia', None, Node('lemon', None, None)), Node('zebra', None, None)))
+        dist_bintree = Node(XYCoords(6, 8), Node(XYCoords(-5, 4), Node(XYCoords(-2, 1), Node(XYCoords(1, -1), None, None), None), Node(XYCoords(-7, -6), Node(XYCoords(3, 4), None, None), None)), Node(XYCoords(-20, -25), Node(XYCoords(10, -11), None, Node(XYCoords(15, 14), None, None)), Node(XYCoords(100, -50), None, None)))
+
+        num_bintree_res = Node(6, Node(5, Node(3, Node(2, None, None), None), Node(4, None, None)), Node(10, Node(8, None, Node(9, None, None)), Node(12, None, None)))
+        str_bintree_res = Node('food', Node('dog', Node('apple', None, None), Node('ear', Node('cat', None, None), None)), Node('yellow', Node('georgia', None, Node('lemon', None, None)), Node('zebra', None, None)))
+        dist_bintree_res = Node(XYCoords(6, 8), Node(XYCoords(-5, 4), Node(XYCoords(-2, 1), Node(XYCoords(1, -1), None, None), None), Node(XYCoords(-7, -6), Node(XYCoords(3, 4), None, None), None)), Node(XYCoords(-20, -25), Node(XYCoords(15, 14), None, None), Node(XYCoords(100, -50), None, None)))
+
+        self.assertEqual(BinarySearchTree(num_ord, num_bintree_res), delete(BinarySearchTree(num_ord, num_bintree), 7))
+        self.assertEqual(BinarySearchTree(str_ord, str_bintree_res), delete(BinarySearchTree(str_ord, str_bintree), 'bingo'))
+        self.assertEqual(BinarySearchTree(dist_ord, dist_bintree_res), delete(BinarySearchTree(dist_ord, dist_bintree), XYCoords(10, -11)))
+
 
 if (__name__ == '__main__'):
     unittest.main()
